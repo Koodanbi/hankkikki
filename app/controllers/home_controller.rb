0818122a7d2f2c301649ac4e197ce7a982579ea9
Koodanbi <Koodanbi@users.noreply.github.com>
2016-08-16 20:38:39 +0900

    Add files via upload

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+    def index
+        
+    end
+    
+    def helpme
+        
+    end
+
+    def together
+        
+    end
+    
+    def login
+        
+    end
+    
+    def signin
+        
+    end
+    
+    def share
+    
+    end
+
+end
